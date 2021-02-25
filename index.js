407f44e56d34c381efa10f8886fca64c3ae68cee
Wilber Ulloa <wilber@fonoma.com>
2021-02-24 22:57:16 -0500

    error handling as middlewares

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 
+require('dotenv').config()
+
+const Note = require('./models/note')
+
 let notes = [
     {
       id: 1,
@@ -40,18 +44,21 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if(note){
-        response.json(note)
-    }
-    else {
-        response.status(404).end()
-    }
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id).then( note => {
+        if (note) {
+            response.json(note)
+        }
+        else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
@@ -68,32 +75,47 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId()
-    }
+    })
 
-    notes = notes.concat(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 
     response.json(note)
 })
 
 app.delete('/api/notes/:id', (request, response) =>{
-    const id = Number(request.param.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()    
+    Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
 })
 
-app.put('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const body = req.body
-  notes = notes.filter(n => n.id !== id)
-  notes = notes.concat(body)
-  res.status(201).json(body)
+app.put('/api/notes/:id', (req, res, next) => {
+    const note = req.body
+    Note.findByIdAndUpdate(req.params.id, note, {new:true})
+    .then( updatedNote => {
+        res.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error)
+
+    if (error.name === 'CastError') {
+        response.status(400).send({error: "malformed id"})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
